b34650f8c85ed934bdf8251cc83cd1f359fb3413
boonyong <boonyong.kang93@gmail.com>
2025-04-20 16:29:58 +0800

    Add TPH mapping strategy example

---
+++ Chapter03/Northwind.Console.HierarchyMapping/Models/HierarchyDb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore; // To use DbSet<T>.
+
+namespace Northwind.Models;
+
+public class HierarchyDb : DbContext
+{
+    public DbSet<Person> People { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+
+    public HierarchyDb(DbContextOptions<HierarchyDb> options)
+        : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Person>().UseTphMappingStrategy();
+
+        // Populate database with sample data. (Seeding)
+        Student person1 = new() { Id = 1, Name = "Roman Roy", 
+            Subject = "History" };
+        Employee person2 = new() { Id = 2, Name = "Kendall Roy", 
+            HireDate = new(year: 2014, month: 4, day: 2) };
+        Employee person3 = new() { Id = 3, Name = "Siobhan Roy", 
+            HireDate = new(year: 2020, month: 9, day: 12) };
+
+        modelBuilder.Entity<Student>().HasData(person1);
+        modelBuilder.Entity<Employee>().HasData(person2, person3);
+    }
+}
---
+++ Chapter03/Northwind.Console.HierarchyMapping/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Data.SqlClient; // To use SqlConnectionStringBuilder.
+using Microsoft.EntityFrameworkCore; // GenerateCreateScript()
+using Northwind.Models; // HierarchyDb, Person, Student, Employee
+
+SqlConnectionStringBuilder conStrBuilder = new()
+{
+    // "ServerName\InstanceName" e.g. @".\sqlexpress".
+    DataSource = ".",
+    InitialCatalog = "HierarchyMapping",
+    TrustServerCertificate = true,
+    MultipleActiveResultSets = true,
+    // Because we want to fail faster. Default is 15 seconds.
+    ConnectTimeout = 3,
+
+    // If using Windows Integrated authentication.
+    IntegratedSecurity = true,
+    //// If using SQL Server authentication.
+    //UserID = Environment.GetEnvironmentVariable("MSSQL_USR"),
+    //Password = Environment.GetEnvironmentVariable("MSSQL_PWD"),
+};
+
+DbContextOptionsBuilder<HierarchyDb> dbCtxOptBuilder = new();
+dbCtxOptBuilder.UseSqlServer(conStrBuilder.ConnectionString);
+
+using HierarchyDb db = new(dbCtxOptBuilder.Options);
+
+bool deleted = await db.Database.EnsureDeletedAsync();
+WriteLine($"Database deleted: {deleted}");
+
+bool created = await db.Database.EnsureCreatedAsync();
+WriteLine($"Database created: {created}");
+WriteLine("SQL script used to create the database:");
+WriteLine(db.Database.GenerateCreateScript());
+
+if (db.Students is null || !db.Students.Any())
+{
+    WriteLine("There are no students.");
+}
+else
+{
+    foreach (Student student in db.Students)
+    {
+        WriteLine("{0} studies {1}", 
+            student.Name, student.Subject);
+    }
+}
+
+if (db.Employees is null || !db.Employees.Any())
+{
+    WriteLine("There are no employees.");
+}
+else
+{
+    foreach (Employee employee in db.Employees)
+    {
+        WriteLine("{0} was hired on {1}", 
+            employee.Name, employee.HireDate);
+    }
+}
