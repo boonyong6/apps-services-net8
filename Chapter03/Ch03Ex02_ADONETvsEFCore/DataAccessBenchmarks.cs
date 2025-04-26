4d7a45b4d8f77bf551579350822d824b9912b530
boonyong <boonyong.kang93@gmail.com>
2025-04-26 23:49:48 +0800

    Benchmark ADO.NET against EF Core

---
+++ Chapter03/Ch03Ex02_ADONETvsEFCore/DataAccessBenchmarks.cs
@@ -0,0 +1,68 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Northwind.EntityModels;
+using System.Data;
+
+public class DataAccessBenchmarks
+{
+    private readonly string _connectionString;
+
+    public DataAccessBenchmarks()
+    {
+        SqlConnectionStringBuilder conStrBuilder = new()
+        {
+            DataSource = ".",
+            InitialCatalog = "Northwind",
+            IntegratedSecurity = true,
+            TrustServerCertificate = true,
+            ConnectTimeout = 3,
+            MultipleActiveResultSets = true,
+        };
+        _connectionString = conStrBuilder.ConnectionString;
+    }
+
+    [Benchmark(Baseline = true)]
+    public async Task<ICollection<Product>> GetAllProductsUsingAdoNetAsync()
+    {
+        using SqlConnection connection = new(_connectionString);
+        await connection.OpenAsync();
+
+        SqlCommand command = connection.CreateCommand();
+        command.CommandText = """
+            SELECT ProductId, ProductName, UnitPrice
+            FROM dbo.Products;
+            """;
+
+        List<Product> products = new();
+
+        using SqlDataReader reader = command.ExecuteReader();
+        while (await reader.ReadAsync())
+        {
+            Product product = new()
+            {
+                ProductId = await reader.GetFieldValueAsync<int>("ProductId"),
+                ProductName = await reader.GetFieldValueAsync<string>("ProductName"),
+                UnitPrice = await reader.GetFieldValueAsync<decimal?>("UnitPrice"),
+            };
+            products.Add(product);
+        }
+
+        return products;
+    }
+
+    [Benchmark]
+    public async Task<ICollection<Product>> GetAllProductsUsingEfCoreAsync()
+    {
+        using NorthwindContext db = new();
+
+        List<Product> products = await db.Products.Select(p => new Product()
+        {
+            ProductId = p.ProductId,
+            ProductName = p.ProductName,
+            UnitPrice = p.UnitPrice,
+        }).ToListAsync();
+
+        return products;
+    }
+}
