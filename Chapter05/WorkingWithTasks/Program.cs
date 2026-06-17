eb3dd256b5cc7d24d321ed9668162cb794bba8f5
boonyong <boonyong.kang93@gmail.com>
2026-06-17 12:54:11 +0800

    Initialize WorkingWithTasks project - Add synchronous execution sample

---
+++ Chapter05/WorkingWithTasks/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics; // To use Stopwatch.
+
+OutputThreadInfo();
+
+Stopwatch timer = Stopwatch.StartNew();
+SectionTitle("Running methods synchronously on one thread.");
+MethodA();
+MethodB();
+MethodC();
+WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
